85a64ecee486c113e4e82b1ebfbd222d26ab96ee
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-05-25 02:34:44 +0600

    payment added

--- src/Components/Dashboard/Payment/Payment.js
+++ src/Components/Dashboard/Payment/Payment.js
@@ -1,5 +1,10 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import PaymentForm from './PaymentForm';
+const stripePromise = loadStripe('pk_test_51L1dg0B5WMGrt28lrlvH9mAZ9Wg6rgSUqLsXpK5DhlJv7SafWEaXSwsOCZ5U4V0voRW4UE3yn9BJaUsF2c8byhjs00Qflj7Vdp');
+
 
 const Payment = () => {
     const [orders, setOrders] = useState([]);
@@ -9,24 +14,25 @@ const Payment = () => {
         .then(res => res.json())
         .then(data => {
             setOrders(data);
-            console.log(data);
+            // console.log(data);
         });
     return (
-        <div className="mt-10">
+        <div className="mt-10 flex gap-10 justify-center items-center">
 
             <div class="card card-compact w-96 bg-base-100 shadow-xl">
                 <figure>
-                    <img src={orders.ProductImg} alt="Shoes" />
+                    <img src={orders.productImg} alt="Shoes" />
                 </figure>
-                <div class="card-body">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
+                <div class="card-body text-left">
+                    <h2 class="card-title">{orders.toolName}</h2>
+                    <p>Quantity: {orders.order}</p>
+                    <p>Price: ${orders.price}</p>
                 </div>
+                
             </div>
-            <h1>Product name: {orders.toolName}</h1>
+            <Elements stripe={stripePromise}>
+                              <PaymentForm orders={orders}></PaymentForm>
+                        </Elements>
         </div>
     );
 };
---
+++ src/Components/Dashboard/Payment/PaymentForm.js
@@ -0,0 +1,113 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useEffect, useState } from 'react';
+
+const PaymentForm = ({orders}) => {
+    const [success, setSuccess] = useState('');
+    const [error, setError] = useState('');
+    const [transactionId, setTransactionId] = useState('');
+    const stripe = useStripe();
+    const elements = useElements();
+    const [clientSecret, setClientSecret] = useState();
+    const {  price, userName, email } = orders;
+
+    useEffect(() => {
+        fetch(' http://localhost:5000/create-payment-intent', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                // 'authorization': `Bearer ${localStorage.getItem('accessTokenForDoctorPortal')}`
+            },
+            body: JSON.stringify({ price })
+        }).then(response => response.json()).then(data => {
+            if (data?.clientSecret) {
+                setClientSecret(data.clientSecret)
+            }
+        })
+    }, [price])
+
+    const handleSubmit = async (event) => {
+        // Block native form submission.
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            // Stripe.js has not loaded yet. Make sure to disable
+            // form submission until Stripe.js has loaded.
+            return;
+        }
+
+        // Get a reference to a mounted CardElement. Elements knows how
+        // to find your CardElement because there can only ever be one of
+        // each type of element.
+        const card = elements.getElement(CardElement);
+
+        if (card == null) {
+            return;
+        }
+
+        // Use your card Element with other Stripe.js APIs
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+
+
+setSuccess('');
+        //  confirm card payment
+        const { paymentIntent, payError} = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: userName,
+                        email: email
+                    },
+                },
+            },
+        );
+
+        if(payError){
+            setError(payError?.message);
+ 
+        }else{
+            setError('');
+            setTransactionId(paymentIntent.id);
+            setSuccess('done');
+            console.log(paymentIntent);
+        }
+    }
+    return (
+        <div>
+        <form onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <button type="submit" disabled={!stripe || !clientSecret}>
+                <button className="btn btn-info"> Pay</button>
+            </button>
+        </form>
+        {error && <p>{error}</p>}
+        {
+            success && <div className='text-green-500'>
+                <p>{success}  </p>
+                <p>Your transaction Id: <span className="text-orange-500 font-bold">{transactionId}</span> </p>
+            </div>
+        }
+    </div>
+    );
+};
+
+export default PaymentForm;
